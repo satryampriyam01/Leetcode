616e8238edad67e7d53de7ca49163baac2f7903e
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-12-05 13:09:20 -0500

    Time: 1219 ms (5.02%) | Memory: 11.8 MB (74.38%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int n = s.length();
+        int prevval = 0, res = 0;
+        long long num = 0;
+        char prevop = '+';
+        for(int i=0; i<n; i++){
+            if(s[i] >='0' && s[i] <='9'){
+                num = num*10 + s[i]-'0';
+            }
+            if(s[i] == '+' || s[i]=='-' || s[i] == '*' || s[i] == '/' || i==n-1){
+                if(prevop == '+'){
+                    res += prevval;
+                    prevval = num;
+                    cout<<res<<" "<<prevval<<endl;
+                }else if(prevop == '-'){
+                    res += prevval;
+                    prevval = -num;
+                }else if(prevop == '*'){
+                    prevval *= num;
+                    cout<<prevval<<endl;
+                }else if(prevop == '/'){
+                    prevval /= num;
+                }
+
+                cout<<prevval<<i<<endl;
+
+                num = 0;
+                prevop = s[i];
+            } 
+        }
+        res += prevval;
+        return res;
+    }
+};
