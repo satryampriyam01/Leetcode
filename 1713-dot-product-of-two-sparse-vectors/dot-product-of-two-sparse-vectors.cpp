c901f518e310e0699d6d3a9684f05e0e6ec477c9
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-12-10 09:20:31 -0500

    Time: 216 ms (6.84%) | Memory: 172.5 MB (43.54%) - LeetSync

---
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.cpp
@@ -0,0 +1,49 @@
+class SparseVector {
+public:
+
+    unordered_map<int, int> amp;
+
+    SparseVector(vector<int> &nums) {
+        
+            for(int i=0;i<nums.size();i++)
+            {
+                if(nums[i]!=0)
+                {
+                    amp[i]=nums[i];
+                }
+            } 
+    }
+
+    int size()
+    {
+        return amp.size();
+    }
+
+    
+    // Return the dotProduct of two sparse vectors
+    int dotProduct(SparseVector& vec) {
+
+        int result=0;
+        for(auto i:vec.amp)
+        { 
+            for(auto j: this->amp)
+            {
+                int r=0;
+                if(i.first==j.first)
+                {
+                    r=i.second*j.second;
+                }
+               //cout<<r<<endl;
+               result+=r;
+            }
+            
+        }
+
+        return result;
+    }
+};
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1(nums1);
+// SparseVector v2(nums2);
+// int ans = v1.dotProduct(v2);
