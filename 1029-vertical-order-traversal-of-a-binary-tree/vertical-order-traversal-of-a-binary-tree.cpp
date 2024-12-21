9f5d5e22a6fc4bf9a8b8aabcd9ced6b532370629
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-12-21 08:55:35 -0500

    Time: 10 ms (5.14%) | Memory: 15.8 MB (25.86%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int mini=0;
+    int maxi=0;
+  unordered_map<int,priority_queue<pair<int,int>>> amp;
+    void helper(TreeNode* root,int x,int y)
+    {
+        if(!root)
+        {
+            return;
+        }
+
+        amp[x].push(make_pair(y,root->val));
+        cout<<x<<" "<<y<<endl;
+        mini=min(x,mini);
+        maxi=max(maxi,x);
+
+        if(root->left)
+        {
+            helper(root->left,x-1,y+1);
+        }
+
+        if(root->right)
+        {
+            helper(root->right,x+1,y+1);
+        }
+
+
+
+    }
+
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        
+        amp.clear();
+        vector<vector<int>> answer;
+        //cout<<mini<<" "<<maxi<<endl;
+        helper(root,0,0);
+
+        for(int i=mini;i<=maxi;i++)
+        {
+            vector<int> result;
+            if(amp[i].size())
+            {
+                priority_queue<pair<int,int>> pq=amp[i];
+                
+                while(pq.size())
+                {
+                    result.push_back(pq.top().second);
+                    pq.pop();
+                }
+            }
+            reverse(result.begin(),result.end());
+            answer.push_back(result);
+        }
+
+        return answer;
+
+    }
+
+};
