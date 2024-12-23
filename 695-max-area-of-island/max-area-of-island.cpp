d81d547997c7f0bd2fee6eb9a28ee430c41c6db8
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-12-23 09:34:15 -0500

    Time: 58 ms (5.02%) | Memory: 31.8 MB (17.77%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+int vst[1001][1001];
+void helper(vector<vector<int>>& grid,int i,int j,int &res)
+{
+    if(i<0 || j<0 || i>grid.size()-1 || j>grid[0].size()-1 || vst[i][j]==1 || grid[i][j]==0)
+    {
+        return;
+    }
+    vst[i][j]=1;
+    res+=1;
+    helper(grid,i+1,j,res);
+    helper(grid,i-1,j,res);
+    helper(grid,i,j+1,res);
+    helper(grid,i,j-1,res);
+}
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+             int maxi=0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                
+                if(grid[i][j]==1 && vst[i][j]==0)
+                {
+                    int res=0;
+                    helper(grid,i,j,res);
+                    maxi=max(maxi,res);
+                }
+            }
+        }
+
+        return maxi;
+    }
+};
