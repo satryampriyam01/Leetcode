4b25043e702ff0c9bc1d05cb31d9173017187f55
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2025-01-10 11:29:38 -0500

    Time: 27 ms (5.49%) | Memory: 16.7 MB (55.87%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,83 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        
+        queue<pair<int,int>> que;
+
+        int n=grid.size();
+        int m=grid[0].size();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    que.push(make_pair(i,j));
+                }
+            }
+        }
+
+        int result=0;
+
+        while(que.size())
+        {
+            int k=que.size();
+            
+            while(k--)
+            {
+                pair<int,int> p=que.front();
+                que.pop();
+                 int i=p.first;
+                 int j=p.second;
+                 cout<<i<<" "<<j<<endl;
+                //cout<<i+1<<" "<<j<<" "<<n<<" "<<grid[i+1][j]<<endl;
+                if(i+1<n && grid[i+1][j]==1)
+                {
+                    grid[i+1][j]=2;
+                    que.push(make_pair(i+1,j));
+                }
+
+                if(i-1>-1 && grid[i-1][j]==1)
+                {
+                   grid[i-1][j]=2;
+                    que.push(make_pair(i-1,j)); 
+                }
+
+                if(j+1<m && grid[i][j+1]==1)
+                {
+                    grid[i][j+1]=2;
+                    que.push(make_pair(i,j+1));
+                }
+
+                if(j-1>-1 && grid[i][j-1]==1)
+                {
+                    grid[i][j-1]=2;
+                    que.push(make_pair(i,j-1)); 
+                }
+           
+            }
+            result++;
+        }
+
+
+    for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    return -1;
+                }
+            }
+        }
+
+if(result)   
+{
+    return result-1;
+}
+return result;
+
+
+    }
+};
