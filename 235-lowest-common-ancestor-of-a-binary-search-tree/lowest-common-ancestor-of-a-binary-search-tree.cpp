47d570837a638b87f79fd5454f133f07eb7d3dbf
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2025-01-14 16:59:24 -0500

    Time: 16 ms (94.65%) | Memory: 23.4 MB (76.14%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -26,8 +26,7 @@ public:
         {
             return lowestCommonAncestor(root->left,p,q);
         }
-
-       if(root->val<p->val && root->val<q->val)
+        else if(root->val<p->val && root->val<q->val)
         {
             return lowestCommonAncestor(root->right,p,q);
         }
