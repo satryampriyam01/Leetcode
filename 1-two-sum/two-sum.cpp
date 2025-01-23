20eefff36f355fd544d66f729cb07d95ac6b9645
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2025-01-23 17:37:56 -0500

    Time: 11 ms (46.97%) | Memory: 17.7 MB (7.42%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -13,7 +13,7 @@ public:
             {
                 result.push_back(amp[target-nums[i]]-1);
                 result.push_back(i);
-                cout<<amp[target-nums[i]]-1<<" "<<i<<endl;
+                //cout<<amp[target-nums[i]]-1<<" "<<i<<endl;
                 //amp[target-nums[i]]=0;
             }
             else
