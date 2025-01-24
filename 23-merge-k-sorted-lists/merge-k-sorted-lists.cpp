9b8e9c56e6c4abf74a363c79edf2eb2372553e4f
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2025-01-23 19:48:11 -0500

    Time: 199 ms (5.27%) | Memory: 18.3 MB (80.90%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -11,72 +11,56 @@
 class Solution {
 public:
 
-
-ListNode * helper_merge(ListNode* left,ListNode* right)
+ListNode* helper_merge(ListNode* left,ListNode* right)
 {
-
-    ListNode *head=new ListNode(-1);
-    ListNode *temp=head;
+    ListNode* head= new ListNode(INT_MIN);
+    ListNode*temp= head;
 
     while(left && right)
     {
-        
-            if(left->val<right->val)
-            {
-                temp->next=left;
-                left=left->next;
-            }
-            else
-            {
-                temp->next=right;
-                right=right->next;
-            }
-
+        if(left->val<right->val)
+        {
+            temp->next=left;
+            left=left->next;
+            
+        }
+        else
+        {
+            temp->next=right;
+            right=right->next;
+        }
         temp=temp->next;
     }
 
+    while(left)
+    {
+            temp->next=left;
+            left=left->next;
+            temp=temp->next;
+    }
 
-            while(left!=NULL)
-            {
-                temp->next=left;
-                left=left->next;
-                 temp=temp->next;
-            }
 
-            while(right!=NULL)
-            {
-                temp->next=right;
-                right=right->next;
-                 temp=temp->next;
-            }
+    while(right)
+    {
+        temp->next=right;
+        right=right->next;
+        temp=temp->next;
+    }
 
     return head->next;
-
 }
+
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
+
         int n=lists.size();
-
-        if(n==0)
-        {
-            return NULL;
-        }
-
-        if(n==1)
-        {
-            return lists[0];
-        }
-
-
-        ListNode *prev= new ListNode(INT_MIN);
-
+        ListNode *prev=new ListNode(INT_MIN);
         for(int i=0;i<n;i++)
         {
             prev=helper_merge(prev,lists[i]);
         }
 
         return prev->next;
-
 
     }
 };
