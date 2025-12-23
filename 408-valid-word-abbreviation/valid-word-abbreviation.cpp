79d2a710270b5791ee034ff85989512fa80e6909
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2025-12-23 08:23:20 -0500

    Time: 0 ms (100.00%) | Memory: 8 MB (55.96%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.cpp
+++ 408-valid-word-abbreviation/valid-word-abbreviation.cpp
@@ -2,64 +2,60 @@ class Solution {
 public:
     bool validWordAbbreviation(string word, string abbr) {
         
-        int m=word.size();
-        int j=0;
-        int n=abbr.size();
 
-        for(int i=0;i<n;)
+        int m=word.size();
+        int n=abbr.size();
+        int i=0;
+        int se=0;
+
+        for(i=0;i<n;)
         {
             int num=0;
-            
+
             if(isdigit(abbr[i]))
             {
-                
                 if(abbr[i]-48==0)
                 {
                    
                     return 0;
-                }
-
+                } 
                 while(isdigit(abbr[i]) && i<n)
+                    { 
+                        int r=abbr[i]-48;
+                        num=num*10+r;
+                        i++;
+                    }
+                   
+                se+=num;
+                if(se>m)
                 {
-                    int r=abbr[i]-48;
-                    num=num*10+r;
-                    i++;
-                }
-                
-                j+=num;
-
-                if(j>m)
-                {
-                    cout<<"point2 j "<<j<<" i "<<i<<endl;
-                    //cout<<word[j]<<" "<<abbr[i]<<endl;
                     return 0;
                 }
-                
             }
             else
             {
-                if(abbr[i]!=word[j])
+                if(word[se]!=abbr[i])
                 {
-                    cout<<abbr[i]<<" "<<word[j]<<endl;
-                    cout<<"point1"<<endl;
+                    cout<<"here"<<endl;
                     return 0;
                 }
                 else
                 {
                     i++;
-                    j++;
+                    se++;
                 }
             }
 
+            
         }
 
-    if(j!=m)
-    {
+        if(se==m)
+        {
+            return 1;
+        }
+        cout<<se<<" "<<i<<endl;
+        cout<<"Here2"<<endl;
         return 0;
-    }
 
-
-
-        return 1;
     }
 };
